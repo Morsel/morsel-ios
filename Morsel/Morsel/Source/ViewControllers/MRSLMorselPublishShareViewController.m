25dd6f47d06a7f7501cf28465b1646e517f1c757
Javier Otero <javier.e.otero@gmail.com>
2014-07-02 15:41:16 -0500

    [Completes #74323264] Instagram syndication is buggy

--- Morsel/Morsel/Source/ViewControllers/MRSLMorselPublishShareViewController.m
+++ Morsel/Morsel/Source/ViewControllers/MRSLMorselPublishShareViewController.m
@@ -173,25 +173,26 @@
 #pragma mark - Private Methods
 
 - (void)sendToInstagram {
-    NSData *photoCroppedData = [[_morsel coverItem] itemPhotoCropped];
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSData *photoCroppedData = [[_morsel coverItem] itemPhotoCropped];
 
-    if (photoCroppedData) {
-        NSString *photoFilePath = [NSString stringWithFormat:@"%@/%@",[NSHomeDirectory() stringByAppendingPathComponent:@"Documents"],@"tempinstgramphoto.igo"];
-        if ([photoCroppedData writeToFile:photoFilePath atomically:YES]) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                _documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:photoFilePath]];
-                _documentInteractionController.UTI = @"com.instagram.exclusivegram";
-                _documentInteractionController.delegate = self;
-                _documentInteractionController.annotation = @{ @"InstagramCaption" : (self.morsel.title ? [NSString stringWithFormat:@"\"%@\" on @eatmorsel", self.morsel.title] : @"Posted on @eatmorsel") };
+        if (photoCroppedData) {
+            NSString *photoFilePath = [NSString stringWithFormat:@"%@/%@",[NSHomeDirectory() stringByAppendingPathComponent:@"Documents"],@"tempinstgramphoto.igo"];
+            if ([photoCroppedData writeToFile:photoFilePath
+                                   atomically:YES]) {
+                self.documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:[NSURL fileURLWithPath:photoFilePath]];
+                self.documentInteractionController.UTI = @"com.instagram.exclusivegram";
+                self.documentInteractionController.delegate = self;
+                self.documentInteractionController.annotation = @{ @"InstagramCaption" : (self.morsel.title ? [NSString stringWithFormat:@"\"%@\" on @eatmorsel", self.morsel.title] : @"Posted on @eatmorsel") };
                 [_documentInteractionController presentOpenInMenuFromRect:CGRectZero
                                                                    inView:self.view
                                                                  animated:YES];
-                
-            });
-        } else {
-            [UIAlertView showAlertViewForErrorString:@"Unable to set Instagram photo. Please try again." delegate:nil];
+            } else {
+                [UIAlertView showAlertViewForErrorString:@"Unable to set Instagram photo. Please try again."
+                                                delegate:nil];
+            }
         }
-    }
+    });
 }
 
 - (void)setOnSwitch:(UISwitch *)socialSwitch
@@ -214,10 +215,10 @@
 
 - (void)documentInteractionController:(UIDocumentInteractionController *)controller
         willBeginSendingToApplication:(NSString *)application {
-    [self.presentingViewController dismissViewControllerAnimated:YES
-                                                      completion:nil];
-
     dispatch_async(dispatch_get_main_queue(), ^{
+        [[NSUserDefaults standardUserDefaults] setInteger:[self.morsel.morselID integerValue]
+                                                   forKey:@"recentlyPublishedMorselID"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
         [[NSNotificationCenter defaultCenter] postNotificationName:MRSLUserDidPublishMorselNotification
                                                             object:_morsel];
     });
@@ -226,6 +227,15 @@
 - (void)documentInteractionControllerDidDismissOpenInMenu:(UIDocumentInteractionController *)controller {
     [self.presentingViewController dismissViewControllerAnimated:YES
                                                       completion:nil];
+}
+
+#pragma mark - Dealloc
+
+- (void)dealloc {
+    if (self.documentInteractionController) {
+        self.documentInteractionController.delegate = nil;
+        self.documentInteractionController = nil;
+    }
 }
 
 @end
