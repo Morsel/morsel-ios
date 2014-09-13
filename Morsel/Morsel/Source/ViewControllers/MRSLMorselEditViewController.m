bb8076609280c206bb42ece45e695e5290b45fa0
Javier Otero <javier.e.otero@gmail.com>
2014-09-12 20:43:12 -0500

    UX fixes

--- Morsel/Morsel/Source/ViewControllers/MRSLMorselEditViewController.m
+++ Morsel/Morsel/Source/ViewControllers/MRSLMorselEditViewController.m
@@ -433,7 +433,7 @@ MRSLMorselEditItemTableViewCellDelegate>
                 [[(MRSLMorselInfoTableViewCell *)tableViewCell titleLabel] setText:(!_morsel || [_morsel hasPlaceholderTitle]) ? @"Name your morsel" : [_morsel title]];
             } else if (indexPath.row == 1) {
                 [[(MRSLMorselInfoTableViewCell *)tableViewCell keyLabel] setText:@"Where:"];
-                [[(MRSLMorselInfoTableViewCell *)tableViewCell titleLabel] setText:[_morsel.place name] ?: @"None"];
+                [[(MRSLMorselInfoTableViewCell *)tableViewCell titleLabel] setText:[_morsel.place name] ?: @"None / Personal"];
             }
             [(MRSLMorselInfoTableViewCell *)tableViewCell  alignLabels];
         }
